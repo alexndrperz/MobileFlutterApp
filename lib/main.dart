e418783395a98a139ae27567fa3772bb846fa99f
alexndrperz <alan80perez@gmail.com>
2024-01-26 11:56:53 -0400

    feat: Container and appbar syling

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,49 @@
-import 'package:code_qr_app/form_widg.dart';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CodeQRApp());
+  runApp(const MainApp());
 }
 
-class CodeQRApp extends StatelessWidget {
-  const CodeQRApp({Key? key}) : super(key: key);
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Kevin es Gay '),
-            centerTitle: true,
-            backgroundColor: Color.fromARGB(255, 174, 174, 174),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[50],
+        appBar: AppBar(
+          elevation: 20.0,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 25,
           ),
-          body: const FormWidg(),
-        ));
+          shadowColor: Colors.black,
+          title: Text(
+            "Sistem",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: false,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Colors.blue[300]),
+              height: 300,
+              width: 300,
+              child: Icon(
+                Icons.face,
+                color: Colors.white,
+                size: 64,
+              )),
+        ),
+      ),
+    );
   }
 }

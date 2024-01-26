6b150664ba10d2910ac6099de4c5dc902d0ec64e
alexndrperz <alan80perez@gmail.com>
2024-01-26 12:29:37 -0400

    feat: Set scrollable elements

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,63 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue[50],
-        appBar: AppBar(
-          elevation: 20.0,
-          leading: Icon(
-            Icons.menu,
-            color: Colors.white,
-            size: 25,
+          backgroundColor: Colors.blue[50],
+          appBar: AppBar(
+            elevation: 20.0,
+            leading: Icon(
+              Icons.menu,
+              color: Colors.white,
+              size: 35,
+            ),
+            shadowColor: Colors.black,
+            title: Text(
+              "Sistem",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            centerTitle: false,
+            backgroundColor: Colors.blue,
           ),
-          shadowColor: Colors.black,
-          title: Text(
-            "Sistem",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: false,
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-          child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  color: Colors.blue[300]),
-              height: 300,
-              width: 300,
-              child: Icon(
-                Icons.face,
-                color: Colors.white,
-                size: 64,
-              )),
-        ),
-      ),
+          body: ListView(
+            children: [
+              Container(
+                height: 200,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue[400],
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue[200],
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue[400],
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue[200],
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue[400],
+              ),
+              Container(
+                height: 200,
+                color: Colors.blue[200],
+              )
+            ],
+          )),
     );
   }
 }
